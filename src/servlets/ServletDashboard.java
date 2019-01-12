b911715cdf18f960fd44a774312d2b9f109c2149
oliverheib <oliverheib@yahoo.co.uk>
2019-01-12 16:02:03 +0000

    final

--- src/servlets/ServletDashboard.java
+++ src/servlets/ServletDashboard.java
@@ -128,20 +128,14 @@ public class ServletDashboard extends HttpServlet
 	private String GenHash(String pass)
 	{
 		try {
-            // Create MessageDigest instance for MD5
             MessageDigest md = MessageDigest.getInstance("MD5");
-            //Add password bytes to digest
             md.update(pass.getBytes());
-            //Get the hash's bytes
             byte[] bytes = md.digest();
-            //This bytes[] has bytes in decimal format;
-            //Convert it to hexadecimal format
             StringBuilder sb = new StringBuilder();
             for(int i=0; i< bytes.length ;i++)
             {
                 sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
             }
-            //Get complete hashed password in hex format
             return sb.toString();
         }
         catch (NoSuchAlgorithmException e)
